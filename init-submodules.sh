f011d725f63aa10f640f6c65253014623d465d94
Wen Zhong(Consultant) <wenzhong@qti.qualcomm.com>
2026-01-12 13:04:32 +0800

    update

--- init-submodules.sh
+++ init-submodules.sh
@@ -2,6 +2,7 @@
 # 初始化所有子模块
 git submodule init
 cd libs/libfdt
+git clone --filter=blob:none --no-checkout git@github.com:dgibson/dtc.git .
 echo "libfdt/" > .git/info/sparse-checkout
 git config core.sparseCheckout true
 git pull origin main  # main是子模块的主分支，根据实际分支调整
